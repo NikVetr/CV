2b981a8657cd9db771af4e4cf789a2e87456cc9c
NikVetr <nlashinsky@ucdavis.edu>
2024-08-14 10:52:47 -0700

    update to merge script

--- merge.sh
+++ merge.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Set the filenames
-resume="vetr_resume_1pg.pdf"
+resume="vetr_resume_2pg.pdf"
 cv="vetr_cv.pdf"
+blank="blank.pdf"
 output="vetr_resume+CV.pdf"
 
 # Check if the files exist
-if [[ -f "$resume" && -f "$cv" ]]; then
+if [[ -f "$resume" && -f "$cv"  && -f "$blank" ]]; then
     # Merge the PDFs
-    pdfunite "$resume" "$cv" "$output"
+    pdfunite "$resume" "$blank" "$cv" "$output"
     echo "PDFs merged successfully into $output"
 else
     echo "One or both PDF files are missing!"
